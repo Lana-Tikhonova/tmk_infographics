b9773d1dfb7459ddd380918f6fcb96a73596c15c
Svetlana <frontend.lana@gmail.com>
2024-04-27 15:57:12 +0500

    fix

--- js/common.js
+++ js/common.js
@@ -1,84 +1,84 @@
- //Заглушка для мобилок
- const infographicPlug = document.querySelector('.infographic_mobile');
+//  //Заглушка для мобилок
+//  const infographicPlug = document.querySelector('.infographic_mobile');
 
- function getWindowOrientation(element) {
-     if (window.innerWidth < 600) {
-         element.classList.add('show');
-     } else {
-         element.classList.remove('show');
-     }
- }
+//  function getWindowOrientation(element) {
+//      if (window.innerWidth < 600) {
+//          element.classList.add('show');
+//      } else {
+//          element.classList.remove('show');
+//      }
+//  }
 
- window.addEventListener('load', (e) => {
+window.addEventListener('load', (e) => {
 
-     var rellax = new Rellax('.rellax');
+    var rellax = new Rellax('.rellax');
 
-     let offset
-     if (window.innerWidth > 991) {
-         offset = 150;
-     } else {
-         offset = 20;
-     }
+    let offset
+    if (window.innerWidth > 991) {
+        offset = 150;
+    } else {
+        offset = 20;
+    }
 
-     AOS.init({
-         easing: 'ease',
-         delay: 100,
-         once: true,
-         duration: 1000,
-         offset: offset,
-         //  disable: 'mobile'
-     });
+    AOS.init({
+        easing: 'ease',
+        delay: 100,
+        once: true,
+        duration: 1000,
+        offset: offset,
+        //  disable: 'mobile'
+    });
 
-     var scene = document.querySelectorAll('.parallax');
-     if (scene) {
-         scene.forEach(element => {
-             var parallaxInstance = new Parallax(element)
-         });
-     }
+    var scene = document.querySelectorAll('.parallax');
+    if (scene) {
+        scene.forEach(element => {
+            var parallaxInstance = new Parallax(element)
+        });
+    }
 
-     const body = document.querySelector('body');
-     let getScrollWidth = () => window.innerWidth - document.documentElement.offsetWidth;
-     let browserScrollWidth = getScrollWidth();
+    const body = document.querySelector('body');
+    let getScrollWidth = () => window.innerWidth - document.documentElement.offsetWidth;
+    let browserScrollWidth = getScrollWidth();
 
-     document.addEventListener('click', (e) => {
-         const target = e.target;
-         if (target.closest('[data-open-modal]')) {
-             e.preventDefault();
-             const targetId = target.closest('[data-open-modal]').getAttribute('href');
-             const selectedModal = document.querySelector('[data-modal]');
-             const modalText = document.querySelectorAll('.modal_body');
-             const selectedModalText = document.querySelector(`[id="${targetId}"]`);
-             selectedModal.classList.add('show');
-             modalText.forEach(item => {
-                 item.classList.remove('show');
-             })
-             selectedModalText.classList.add('show');
-             body.classList.add('locked');
-             if (getScrollWidth() == 0) {
-                 body.style.paddingRight = `${browserScrollWidth}px`;
-             }
-         }
+    document.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target.closest('[data-open-modal]')) {
+            e.preventDefault();
+            const targetId = target.closest('[data-open-modal]').getAttribute('href');
+            const selectedModal = document.querySelector('[data-modal]');
+            const modalText = document.querySelectorAll('.modal_body');
+            const selectedModalText = document.querySelector(`[id="${targetId}"]`);
+            selectedModal.classList.add('show');
+            modalText.forEach(item => {
+                item.classList.remove('show');
+            })
+            selectedModalText.classList.add('show');
+            body.classList.add('locked');
+            if (getScrollWidth() == 0) {
+                body.style.paddingRight = `${browserScrollWidth}px`;
+            }
+        }
 
-         function closeModal() {
-             e.preventDefault();
-             document.querySelector('.modal.show').classList.remove('show');
-             body.classList.remove('locked');
-             document.querySelector('.modal_body.show').classList.remove('show');
-             body.style.paddingRight = ``;
-         }
-         if (target.closest('[data-modal-close]')) {
-             closeModal()
-         }
-         if (target.closest('.modal') && !target.closest('.modal-content')) {
-             closeModal()
-         }
-     });
+        function closeModal() {
+            e.preventDefault();
+            document.querySelector('.modal.show').classList.remove('show');
+            body.classList.remove('locked');
+            document.querySelector('.modal_body.show').classList.remove('show');
+            body.style.paddingRight = ``;
+        }
+        if (target.closest('[data-modal-close]')) {
+            closeModal()
+        }
+        if (target.closest('.modal') && !target.closest('.modal-content')) {
+            closeModal()
+        }
+    });
 
-     //Заглушка для мобилок
-     if (infographicPlug) {
-         getWindowOrientation(infographicPlug);
-         window.addEventListener('resize', (e) => {
-             getWindowOrientation(infographicPlug);
-         });
-     }
- });
+    //  //Заглушка для мобилок
+    //  if (infographicPlug) {
+    //      getWindowOrientation(infographicPlug);
+    //      window.addEventListener('resize', (e) => {
+    //          getWindowOrientation(infographicPlug);
+    //      });
+    //  }
+});
